43da3ce2768dbd9e0139b377cb04e21ec7e3acbf
Chris Droukas <chris.d@socialcodeinc.com>
2016-03-07 15:29:18 -0500

    Update BugShaker.swift

--- Pod/Classes/BugShaker.swift
+++ Pod/Classes/BugShaker.swift
@@ -10,140 +10,143 @@ import MessageUI
 
 
 public class BugShaker {
-  
-  struct Config {
-    static var toRecipients: [String]?
-    static var subject: String?
-    static var body: String?
-  }
-  
-  // MARK: - Configuration
-  
-  /**
-  Set bug report email recipient(s), custom subject line and body.
-  
-  - parameter toRecipients: List of email addresses to which the report will be sent.
-  - parameter subject:      Custom subject line to use for the report email.
-  - parameter body:         Custom email body (plain text).
-  */
-  public class func configure(to toRecipients: [String]!, subject: String?, body: String?) {
-    Config.toRecipients = toRecipients
-    Config.subject = subject
-    Config.body = body
-  }
-  
+    
+    struct Config {
+        static var toRecipients: [String]?
+        static var subject: String?
+        static var body: String?
+    }
+    
+    // MARK: - Configuration
+    
+    /**
+     Set bug report email recipient(s), custom subject line and body.
+     
+     - parameter toRecipients: List of email addresses to which the report will be sent.
+     - parameter subject:      Custom subject line to use for the report email.
+     - parameter body:         Custom email body (plain text).
+     */
+    public class func configure(to toRecipients: [String]!, subject: String?, body: String?) {
+        Config.toRecipients = toRecipients
+        Config.subject = subject
+        Config.body = body
+        NSUserDefaults.standardUserDefaults().setObject("\(true)", forKey: "bugShakerEnabled")
+    }
 }
 
 extension UIViewController: MFMailComposeViewControllerDelegate {
-  
-  // MARK: - UIResponder
-  
-  override public func canBecomeFirstResponder() -> Bool {
-    return true
-  }
-  
-  override public func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
-    if motion == .MotionShake {
-      let cachedScreenshot = captureScreenshot()
-      
-      presentReportPrompt({ (action) -> Void in
-        self.presentReportComposeView(cachedScreenshot)
-      })
-    }
-  }
-  
-  // MARK: - Alert
-  
-  func presentReportPrompt(reportActionHandler: (UIAlertAction) -> Void) {
-    let actionSheet = UIAlertController(title: "Shake detected!", message: "Would you like to report a bug?", preferredStyle: .ActionSheet)
     
-    let reportAction = UIAlertAction(title: "Report A Bug", style: .Default, handler: reportActionHandler)
-    let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { _ in }
+    // MARK: - UIResponder
     
-    actionSheet.addAction(reportAction)
-    actionSheet.addAction(cancelAction)
-    
-    presentViewController(actionSheet, animated: true, completion: nil)
-  }
-  
-  
-  // MARK: - Report methods
-  
-  /**
-  Take a screenshot for the current screen state.
-  
-  - returns: Screenshot image.
-  */
-  func captureScreenshot() -> UIImage? {
-    var screenshot: UIImage? = nil
-    
-    if let layer = UIApplication.sharedApplication().keyWindow?.layer {
-      let scale = UIScreen.mainScreen().scale
-      
-      UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
-      
-      if let context = UIGraphicsGetCurrentContext() {
-        layer.renderInContext(context)
-      }
-      
-      screenshot = UIGraphicsGetImageFromCurrentImageContext()
-      
-      UIGraphicsEndImageContext()
+    override public func canBecomeFirstResponder() -> Bool {
+        return true
     }
     
-    return screenshot;
-  }
-  
-  /**
-   Present the user with a mail compose view with the recipient(s), subject line and body
-   pre-populated, and the screenshot attached.
-   
-   - parameter screenshot: The screenshot to attach to the report.
-   */
-  func presentReportComposeView(screenshot: UIImage?) {
-    if MFMailComposeViewController.canSendMail() {
-      let mailComposer = MFMailComposeViewController()
-      
-      guard let toRecipients = BugShaker.Config.toRecipients else {
-        print("BugShaker – Error: No recipients provided. Make sure that BugShaker.configure() is called.")
-        return
-      }
-      
-      mailComposer.setToRecipients(toRecipients)
-      mailComposer.setSubject(BugShaker.Config.subject ?? "Bug Report")
-      mailComposer.setMessageBody(BugShaker.Config.body ?? "", isHTML: false)
-      mailComposer.mailComposeDelegate = self
-      
-      if let screenshot = screenshot, let screenshotJPEG = UIImageJPEGRepresentation(screenshot, CGFloat(1.0)) {
-        mailComposer.addAttachmentData(screenshotJPEG, mimeType: "image/jpeg", fileName: "screenshot.jpeg")
-      }
-      
-      presentViewController(mailComposer, animated: true, completion: nil)
-    }
-  }
-  
-  // MARK: - MFMailComposeViewControllerDelegate
-  
-  public func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
-    if let error = error {
-      print("BugShaker – Error: \(error)")
+    override public func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        
+        if (NSUserDefaults.standardUserDefaults().stringForKey("bugShakerEnabled") == "\(true)") {
+            if motion == .MotionShake {
+                let cachedScreenshot = captureScreenshot()
+                
+                presentReportPrompt({ (action) -> Void in
+                    self.presentReportComposeView(cachedScreenshot)
+                })
+            }
+        }
     }
     
-    switch result {
-    case MFMailComposeResultFailed:
-      print("BugShaker – Bug report send failed.")
-      break;
-      
-    case MFMailComposeResultSent:
-      print("BugShaker – Bug report sent!")
-      break;
-      
-    default:
-      // noop
-      break;
+    // MARK: - Alert
+    
+    func presentReportPrompt(reportActionHandler: (UIAlertAction) -> Void) {
+        let actionSheet = UIAlertController(title: "Shake detected!", message: "Would you like to report a bug?", preferredStyle: .ActionSheet)
+        
+        let reportAction = UIAlertAction(title: "Report A Bug", style: .Default, handler: reportActionHandler)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { _ in }
+        
+        actionSheet.addAction(reportAction)
+        actionSheet.addAction(cancelAction)
+        
+        presentViewController(actionSheet, animated: true, completion: nil)
     }
     
-    dismissViewControllerAnimated(true, completion: nil)
-  }
-  
+    
+    // MARK: - Report methods
+    
+    /**
+     Take a screenshot for the current screen state.
+     
+     - returns: Screenshot image.
+     */
+    func captureScreenshot() -> UIImage? {
+        var screenshot: UIImage? = nil
+        
+        if let layer = UIApplication.sharedApplication().keyWindow?.layer {
+            let scale = UIScreen.mainScreen().scale
+            
+            UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, scale);
+            
+            if let context = UIGraphicsGetCurrentContext() {
+                layer.renderInContext(context)
+            }
+            
+            screenshot = UIGraphicsGetImageFromCurrentImageContext()
+            
+            UIGraphicsEndImageContext()
+        }
+        
+        return screenshot;
+    }
+    
+    /**
+     Present the user with a mail compose view with the recipient(s), subject line and body
+     pre-populated, and the screenshot attached.
+     
+     - parameter screenshot: The screenshot to attach to the report.
+     */
+    func presentReportComposeView(screenshot: UIImage?) {
+        if MFMailComposeViewController.canSendMail() {
+            let mailComposer = MFMailComposeViewController()
+            
+            guard let toRecipients = BugShaker.Config.toRecipients else {
+                print("BugShaker – Error: No recipients provided. Make sure that BugShaker.configure() is called.")
+                return
+            }
+            
+            mailComposer.setToRecipients(toRecipients)
+            mailComposer.setSubject(BugShaker.Config.subject ?? "Bug Report")
+            mailComposer.setMessageBody(BugShaker.Config.body ?? "", isHTML: false)
+            mailComposer.mailComposeDelegate = self
+            
+            if let screenshot = screenshot, let screenshotJPEG = UIImageJPEGRepresentation(screenshot, CGFloat(1.0)) {
+                mailComposer.addAttachmentData(screenshotJPEG, mimeType: "image/jpeg", fileName: "screenshot.jpeg")
+            }
+            
+            presentViewController(mailComposer, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: - MFMailComposeViewControllerDelegate
+    
+    public func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        if let error = error {
+            print("BugShaker – Error: \(error)")
+        }
+        
+        switch result {
+        case MFMailComposeResultFailed:
+            print("BugShaker – Bug report send failed.")
+            break;
+            
+        case MFMailComposeResultSent:
+            print("BugShaker – Bug report sent!")
+            break;
+            
+        default:
+            // noop
+            break;
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 }
